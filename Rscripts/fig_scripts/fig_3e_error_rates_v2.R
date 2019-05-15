a4e10d3890d278e4bb84a512bce211bb45b3fdf0
Albert Chen <blahoink@gmail.com>
2019-05-15 01:14:19 -0400

    update axis labels

--- Rscripts/fig_scripts/fig_3e_error_rates_v2.R
+++ Rscripts/fig_scripts/fig_3e_error_rates_v2.R
@@ -82,16 +82,16 @@ abline(a=0, b=1, col='red')
 
 # Plot --------------------------------------------------------------------
 
-pdf(file='manuscript/Figs/error_rate_line_v4.pdf', width=1.75, height=1.5)
+pdf(file='manuscript/Figs/error_rate_line_v5.pdf', width=1.75, height=1.5)
 
 par(mar=c(0.5,2,0.1,0.1), oma=c(1,0,0,0), cex.axis=0.7, pty='s')
 
-plot(0, 0, type='n', xlim=c(-3.3, -0.7), ylim=c(-3.3, -0.7),
+plot(0, 0, type='n', xlim=c(-3.3, -0.7), ylim=c(-3.7, -0.7),
      xlab=NA, ylab=NA, xaxt='n', yaxt='n')
 
 # grid()
 abline(v=log10(fdr_levels), col = "grey70", lty = "dotted", lwd=1)
-abline(h=log10(fdr_levels), col = "grey70", lty = "dotted", lwd=1)
+abline(h=c(log10(0.0003), log10(fdr_levels)), col = "grey70", lty = "dotted", lwd=1)
 
 points(log10(fdr_levels), log10(y_dart), col=cb[2], pch=16, cex=0.8)
 lines(log10(fdr_levels), log10(y_dart), col=cb[2], lwd=2)
@@ -102,7 +102,7 @@ lines(log10(fdr_levels), log10(y_mq), col=cb[1], lwd=2)
 abline(a=0, b=1, col='black', lwd=1, lty=2)
 axis(1, at=log10(fdr_levels), labels=c('0.1%', '0.3%', '1%', '3%', '10%'),
      tck=-0.02, mgp=c(0, -0.15, 0))
-axis(2, at=log10(fdr_levels), labels=c('0.1%', NA, '1%', NA, '10%'),
+axis(2, at=c(log10(0.0003), log10(fdr_levels)), labels=c(NA, '0.1%', NA, '1%', NA, '10%'),
      tck=-0.02, mgp=c(0, 0.2, 0), las=1)
 
 mtext('PSM FDR (q-value)', side=1, line=0.5, cex=0.7, outer=F)
